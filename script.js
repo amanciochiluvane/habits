f7adff088cb9c32b8ad02f10eba79542d8bf260e
Amâncio Chiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2023-01-20 08:49:12 +0200

    solved an issue

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function add() {
   const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
-    alert("Dia já incluso! 🔴")
+    alert("Dia já incluso 🔴")
     return
   }
 
-  alert("Adicionado com sucesso! ✅")
+  alert("Adicionado com sucesso ✅")
   nlwSetup.addDay(today)
 }
 
